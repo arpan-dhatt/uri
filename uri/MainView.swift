ea060e283224a7e0c9eb26c88666187272db9f2c
Saaketh C <Saakethrchenna@gmail.com>
2021-02-20 15:14:25 -0600

    test

---
+++ uri/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  uri
+//
+//  Created by user175936 on 2/20/21.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
