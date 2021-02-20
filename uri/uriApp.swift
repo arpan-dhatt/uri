001da32bc14783bc94a29e472389867f3fff7513
Arpan Dhatt <ary.dhatt@gmail.com>
2021-02-20 14:16:37 -0600

    Initial Commit

---
+++ uri/uriApp.swift
@@ -0,0 +1,17 @@
+//
+//  uriApp.swift
+//  uri
+//
+//  Created by Arpan Dhatt on 2/20/21.
+//
+
+import SwiftUI
+
+@main
+struct uriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
