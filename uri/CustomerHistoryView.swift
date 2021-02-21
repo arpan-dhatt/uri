5b6c56c850964870b45d13f345bcc0340decf62f
Arpan Dhatt <ary.dhatt@gmail.com>
2021-02-20 23:22:27 -0600

    working on customer history view

--- uri/CustomerHistoryView.swift
+++ uri/CustomerHistoryView.swift
@@ -7,4 +7,30 @@
 
 import SwiftUI
 
+struct CustomerHistoryView : View {
+    private enum HistoryType: String, CaseIterable, Identifiable{
+        case current = "Current"
+        case completed = "Completed"
+        
+        var id: String{ self.rawValue }
+    }
+    
+    @State private var historyTypes: HistoryType = .current
+    
+    var body: some View {
+        VStack {
+            Picker(selection: $historyTypes, label: Text(""), content: {
+                ForEach(HistoryType.allCases) {
+                    Text($0.rawValue).font(.title2)
+                }
+            }).pickerStyle(SegmentedPickerStyle())
+            Divider()
+        }
+    }
+}
 
+struct CustomerHistoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomerHistoryView().environmentObject(ViewModel())
+    }
+}
